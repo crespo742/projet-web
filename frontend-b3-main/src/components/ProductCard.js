69332b10cfd010ad5e08e65395df4594b53373d0
killianlecornet <56248842+killianlecornet@users.noreply.github.com>
2022-05-20 14:15:15 +0200

    css

--- frontend-b3-main/src/components/ProductCard.js
+++ frontend-b3-main/src/components/ProductCard.js
@@ -21,22 +21,42 @@ const Productcard = (props) => {
 };
     return (
         <div className="product__card">
-              <div className="product__data">
-                <h2>{props.product.attributes.title}</h2> 
-              <div className="product__img">
-                <img src={`http://localhost:1337${props.product.attributes.image.data.attributes.url}`} alt={props.product.attributes.title} />
-              </div>
-                <p>{props.product.attributes.price} € </p>
-                <p>
-                  <Link href={`/shop/${props.product.id}`}>
-                  {/* <Link href={'/shop/' + props.product.id} */}
-                    <a>
-                      <Button title="Voir le produit" type="button" classes="btn btn__color-black"/>
-                    </a>
-                  </Link>
-                </p>
+          <div class="shell">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-3">
+                  <div class="wsk-cp-product">
+                    <div class="wsk-cp-img">
+                    <img src={`http://localhost:1337${props.product.attributes.image.data.attributes.url}`} alt={props.product.attributes.title} />
+                    </div>
+                    <div class="wsk-cp-text">
+                      <div class="title-product">
+                        <h3>{props.product.attributes.title}</h3>
+                      </div>
+                      <div class="description-prod">
+                        <p>{props.product.attributes.description}</p>
+                      </div>
+                      <div class="card-footer">
+                        <div class="wcf-left"><span class="price">{props.product.attributes.price} €</span></div>
+                        <div class="wcf-right">
+                        <p>
+                            <Link href={`/shop/${props.product.id}`}>
+                            {/* <Link href={'/shop/' + props.product.id} */}
+                              <a>
+                                <Button title="Voir le produit" type="button" classes="btn btn__color-black"/>
+                              </a>
+                            </Link>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
+          </div>
+        </div>
+            
     );
 }
 
--- frontend-b3-main/src/pages/shop/index.js
+++ frontend-b3-main/src/pages/shop/index.js
@@ -25,6 +25,8 @@ const Index = () => {
           ))}
       </div>
     </div>
+    
+    
   );
 };
 
